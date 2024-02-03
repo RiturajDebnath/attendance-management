45fa71bd5a0ed774542fb7d2c18ad485a34e1435
Xron Trix <xrontrix@gmail.com>
2024-02-03 23:05:30 +0530

    Added Page Routing and tailwindCSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './pages/Login';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          {/* <Route path="contact" element={<Contact />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
